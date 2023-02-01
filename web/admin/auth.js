36a54d9e4a3f5040265e2b7c2f1f8c5246cff060
Ahmad Zacky <zackfxg@gmail.com>
2023-02-01 15:01:01 +0700

    auth axios

--- web/admin/auth.js
+++ web/admin/auth.js
@@ -1,16 +1,12 @@
-fetch(
-  "/PerpusJava/AuthController?" +
-    new URLSearchParams({
-      page: "cek",
-    })
-)
-  .then((res) => {
-    console.log("res : " + JSON.stringify(res));
-    res.json();
-  })
+axios
+  .get(
+    "/PerpusJava/AuthController?" +
+      new URLSearchParams({
+        page: "cek",
+      })
+  )
   .then((data) => {
-    console.log(data);
-    if (data.status === "NO") {
+    if (data.data.status === "NO") {
       window.location.href = "/PerpusJava/admin/pages/samples/login.html";
     }
   });
